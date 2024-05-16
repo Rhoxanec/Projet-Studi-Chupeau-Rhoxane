ac315403afcb3f2b941b086dae62915d8359acef
Rhoxane Chupeau <rhoxane.chupeau@gmail.com>
2024-05-16 19:00:24 +0200

    V2 Enregistrement (maj url)

--- Authentification/jo_users/jo_users/urls.py
+++ Authentification/jo_users/jo_users/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from auth_app import views
 
 urlpatterns = [
+    path('', views.accueil, name='accueil'),
     path('admin/', admin.site.urls),
     path('', views.inscription, name='inscription'),
-    path('connexion/', views.connexion, name='connexion'),
     path('accueil/', views.accueil, name= 'accueil'),
+    path('billetterie/', views.billetterie, name='billetterie'),
+    path('moncompte/', views.moncompte, name='moncompte'),
+    path('panier/', views.panier, name='panier'),
+    path('connexion/', views.connexion, name='connexion'),
     path('deconnexion/', views.deconnexion, name='deconnexion'),
 ]
