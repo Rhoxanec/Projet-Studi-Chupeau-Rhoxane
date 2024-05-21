56eee5637dc2af656684c215edba2add5677da66
Rhoxane Chupeau <rhoxane.chupeau@gmail.com>
2024-05-21 23:10:20 +0200

    Correction page inscription

--- ProjetJO2024/website/form.py
+++ ProjetJO2024/website/form.py
@@ -51,7 +51,7 @@ class CustomUserCreationForm (UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
@@ -61,10 +61,10 @@ class CustomUserCreationForm (UserCreationForm):
         self.fields['username'].label = ''
         self.fields['username'].help_text = '<span class="form-text text-muted"><small>Requis: votre nom utilisateur doit être votre nom de famille suivi de votre prénom.</small></span>'
 
-        self.fields['password'].widget.attrs['class'] = 'form-control'
-        self.fields['password'].widget.attrs['placeholder'] = 'Mot de passe'
-        self.fields['password'].label = ''
-        self.fields['password'].help_text = '<ul class="form-text text-muted small"><li>Votre mot de passe ne doit pas être trop similaire à vos informations personnelles.</li><li>Votre mot de passe doit contenir au moins 8 caractères contenant des lettres, des chiffres et des @/./+/-/_ seulement.</li><li>Votre mot de passe ne doit pas être un mot de passe commun (ex: maman).</li></ul>'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Mot de passe'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Votre mot de passe ne doit pas être trop similaire à vos informations personnelles.</li><li>Votre mot de passe doit contenir au moins 8 caractères contenant des lettres, des chiffres et des @/./+/-/_ seulement.</li><li>Votre mot de passe ne doit pas être un mot de passe commun (ex: maman).</li></ul>'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirmer le mot de passe'
